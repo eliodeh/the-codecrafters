2267e614eff3ceca0ceaa84c513401a0e1c01ff4
eliodeh <odehe359@gmail.com>
2026-03-27 15:53:23 +0100

    mysecond commit

--- hackerthon-day1/main.go
+++ hackerthon-day1/main.go
@@ -5,44 +5,24 @@ import (
 )
 
 func main() {
-	const (
-		colourred    = "\033[0;31m"
-		colouryellow = "\033[0;33m"
-		colourblue   = "\033[0;34m"
-		colourpurple = "\033[0;35m"
-		colourreset  = "\033[0m"
-		colourgreen  = "\033[0;32m"
-	)
 	fmt.Println("CLI Calculator")
 	for {
-		var firstnum int
-		var secondnum int
+		var firstnum float64
+		var secondnum float64
 		var Operate string
-		var digit string
 
-		fmt.Println(colourred + "enter any firstnum" + colourreset)
-		fmt.Scan(&firstnum)
-		if _, err := fmt.Scan(&firstnum); err != nil {
-			fmt.Println("invalid number")
-			continue
-		}
-		fmt.Println(colourgreen + "enter any secondnum" + colourreset)
-		fmt.Scan(&secondnum)
-		if _, err := fmt.Scan(&secondnum); err != nil {
-			fmt.Println("invalid number")
-			continue
+		start:
+		fmt.Println("enter any firstnum")
+		_, err1 := fmt.Scan(&firstnum)
+		fmt.Println("enter any secondnum")
+		_, err2 := fmt.Scan(&secondnum)
+
+		if err1 != nil || err2 != nil {
+			fmt.Println("Input an valid  number")
+			goto start
 		}
 		fmt.Println("enter any Operate add, sub, mul, div, help, quit")
 		fmt.Scan(&Operate)
-		if _, err := fmt.Scan(&Operate); err != nil {
-			fmt.Println("invalid number")
-			continue
-		}
-		fmt.Println("enter any digit")
-		fmt.Scan(&digit)
-		if _, err := fmt.Scan(&digit); err != nil {
-			fmt.Println("invalid number")
-		}
 
 		if Operate == "add" {
 			fmt.Println(add(firstnum, secondnum))
@@ -57,35 +37,30 @@ func main() {
 			fmt.Println(div(firstnum, secondnum))
 		}
 		if Operate == "help" {
-			fmt.Println("Help ")
+			fmt.Println("add, sub, mul, div, help, quit")
 		}
 		if Operate == "quit" {
 			fmt.Println("Goodbye codecrafterthon")
 			break
 		}
-		fmt.Scan(Operate)
-		fmt.Println(add(firstnum, secondnum))
-		fmt.Println(sub(firstnum, secondnum))
-		fmt.Println(mul(firstnum, secondnum))
-		fmt.Println(div(firstnum, secondnum))
 	}
 }
 
 // the running functions of the maths //
-func add(fnum, snum int) int {
+func add(fnum, snum float64) float64 {
 	return fnum + snum
 }
-func sub(fnum, snum int) int {
+func sub(fnum, snum float64) float64 {
 	return fnum - snum
 }
-func mul(fnum, snum int) int {
+func mul(fnum, snum float64) float64 {
 	return fnum * snum
 }
-func div(fnum, snum int) int {
+func div(fnum, snum float64) float64 {
 	if snum == 0 {
-		fmt.Print("can't divide by 0")
+		fmt.Print("can't divide by ")
 	} else {
 		return fnum / snum
 	}
-	return 3
+	return 0
 }
